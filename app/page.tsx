ff57bf35a1c4709234fa287ff9ff9ca7c8ae0778
Raymund Lew <raymund.lew@gmail.com>
2023-07-18 17:41:59 -1000

    validation, success/fail states on submitting

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 'use client'
 
 import React, { useState, CSSProperties } from 'react'
-import { Button, Modal, Layout, Space, Col, Row, Form, Input, Checkbox, Typography, Select } from 'antd'
+import {
+  Button, Modal, Layout, Space, Col, Row, Form,
+  Input, Checkbox, Typography, Select, message
+} from 'antd'
 import CylinderShape from './components/cylindershape'
 import ModalCheckItems from './components/modalcheckitems'
 import './page.css'
@@ -16,38 +19,79 @@ const modalCss: CSSProperties = {
   minWidth: '50%'
 }
 
+interface formValues {
+  firstName: string
+  lastName: string
+  email: string
+  countryCode: string
+  phone: string
+  whatsappUpdates: boolean
+}
+
 const Home: React.FC = () => {
-  const [modalLoading, setModalLoading] = useState(false)
+  const [submitLoading, setSubmitLoading] = useState(false)
   const [modalOpen, setModalOpen] = useState(false)
   const [form] = Form.useForm()
+  const [messageApi, contextHolder] = message.useMessage()
 
   const showModal = () => {
     setModalOpen(true)
-  };
-
-  const handleOk = () => {
-    setModalLoading(true)
-    setTimeout(() => {
-      setModalLoading(false)
-      setModalOpen(false)
-    }, 3000)
   }
 
   const handleCancel = () => {
     setModalOpen(false)
   }
 
-  const onFinish = (values: any) => {
-    console.log('Received values of form: ', values)
+  const randomIntBetween = (min: number, max: number) => {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min) + min)
   }
 
-  const prefixSelector = (
-    <Form.Item name="prefix" noStyle initialValue={'1'}>
+  const onFinish = (values: formValues) => {
+    console.log('Received values of form: ', values)
+    setSubmitLoading(true)
+    setTimeout(() => {
+      setSubmitLoading(false)
+      setModalOpen(false)
+
+      // 30% chance of randomly failing :)
+      randomIntBetween(0, 100) < 70 ? success() : failure()
+    }, randomIntBetween(500, 2500))
+  }
+
+  const success = () => {
+    messageApi.open({
+      type: 'success',
+      content: 'Values submitted successfully. Check the console!',
+      duration: 5
+    })
+  }
+
+  const failure = () => {
+    messageApi.open({
+      type: 'error',
+      content: "Oh no! Something very randomly went wrong! And it's ambiguous what it is!",
+      duration: 5
+    })
+  }
+
+  const countryCodeSelector = (
+    <Form.Item name="countryCode" noStyle initialValue={'1'}>
       <Select style={{ width: 70 }}>
-        <Option value="1">+1</Option>
+        <Option value='1'>+1</Option>
       </Select>
     </Form.Item>
-  );
+  )
+
+  const onSubmit = () => {
+    console.log('submitted, waiting 3 sec')
+    setSubmitLoading(true)
+    setTimeout(() => {
+      setSubmitLoading(false)
+      setModalOpen(false)
+    }, 3000)
+  }
 
   return (
     <Space
@@ -56,6 +100,7 @@ const Home: React.FC = () => {
       align="center"
       className="page-container"
     >
+      { contextHolder }
       <Content>
         <Button type="primary" onClick={showModal} size="large">
           CLICK ME!
@@ -63,7 +108,6 @@ const Home: React.FC = () => {
 
         <Modal
           open={modalOpen}
-          onOk={handleOk}
           style={modalCss}
           onCancel={handleCancel}
           centered
@@ -72,10 +116,14 @@ const Home: React.FC = () => {
         >
           <Row>
             <Col
-              xs={24} sm={24} md={12} lg={12} xl={12}
+              xs={24}
+              sm={24}
+              md={12}
+              lg={12}
+              xl={12}
               className="modal-infographics"
             >
-              <CylinderShape className='cylinder-shape'></CylinderShape>
+              <CylinderShape className="cylinder-shape"></CylinderShape>
               <Space direction="vertical" size="large">
                 <Text className="modal-title">Talk to a career expert</Text>
                 <Title>Schedule 1:1 free counselling</Title>
@@ -86,79 +134,133 @@ const Home: React.FC = () => {
               </Space>
             </Col>
             <Col
-              xs={24} sm={24} md={12} lg={12} xl={12}
+              xs={24}
+              sm={24}
+              md={12}
+              lg={12}
+              xl={12}
               className="modal-form-container"
             >
-              <Form form={form} id='signup-form' name="signup-form" onFinish={onFinish}>
-                <Space direction="vertical" size="large">
-                  <Row gutter={12}>
-                    <Space size='small'>
-                      <Col>
-                        <Input type="string" placeholder="First Name*"></Input>
-                      </Col>
-                      <Col>
-                        <Input type="string" placeholder="Last Name*"></Input>
-                      </Col>
-                    </Space>
-                  </Row>
-                  <Row>
+              <Form
+                form={form}
+                id="signup-form"
+                name="signup-form"
+                onFinish={onFinish}
+                scrollToFirstError
+              >
+                <Row gutter={12}>
+                  <Space size="small">
                     <Col>
-                      <Input type="email" placeholder="Email*"></Input>
+                      <Form.Item
+                        name="firstName"
+                        rules={[
+                          { type: "string" },
+                          {
+                            required: true,
+                            message: "First name is required.",
+                          },
+                        ]}
+                      >
+                        <Input
+                          type="string"
+                          placeholder="First Name*"
+                          required
+                        />
+                      </Form.Item>
                     </Col>
-                  </Row>
-                  <Form.Item
-                    name="phone"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Phone number is required.",
-                      },
-                    ]}
-                  >
-                    <Input
-                      addonBefore={prefixSelector}
-                      style={{ width: "100%" }}
-                      type='phone'
-                    />
-                  </Form.Item>
-                  <Form.Item>
-                    <Checkbox
-                      style={{
-                        fontSize: '12px',
-                        color: '#969696'
-                      }}
-                      checked
-                    >
-                      I want to receive updates directly on WhatsApp.
-                    </Checkbox>
-                  </Form.Item>
-                  <Form.Item>
-                    <Button
-                      type="primary"
-                      htmlType="submit"
-                      style={{
-                        height: '3.5rem',
-                        background: 'linear-gradient(to right, #b132bc 0%, #e4753b 100%'
-                      }}
-                      size='large'
-                      block
-                    >
-                      Submit
-                    </Button>
-                  </Form.Item>
-                  <Text
+                    <Col>
+                      <Form.Item
+                        name="lastName"
+                        rules={[
+                          { type: "string" },
+                          {
+                            required: true,
+                            message: "Last name is required.",
+                          },
+                        ]}
+                      >
+                        <Input type="string" placeholder="Last Name*"></Input>
+                      </Form.Item>
+                    </Col>
+                  </Space>
+                </Row>
+                <Form.Item
+                  name='email'
+                  rules={[
+                    {
+                      type: "email",
+                      message: "Please enter a valid email.",
+                    },
+                    {
+                      required: true,
+                      message: "Email is required.",
+                    },
+                  ]}
+                >
+                  <Input type="email" placeholder="Email*"></Input>
+                </Form.Item>
+                <Form.Item
+                  name="phone"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Phone number is required.",
+                    },
+                  ]}
+                >
+                  <Input
+                    addonBefore={countryCodeSelector}
+                    style={{ width: "100%" }}
+                    type="phone"
+                  />
+                </Form.Item>
+                <Form.Item
+                  name='whatsappUpdates'
+                  valuePropName='checked'
+                  initialValue={true}
+                >
+                  <Checkbox
                     style={{
-                      fontSize: '12px',
-                      textAlign: 'center',
-                      color: '#969696'
+                      fontSize: "12px",
+                      color: "#969696",
                     }}
+                    checked
                   >
-                    By tapping submit, you agree to KnowledgeHut{' '}
-                    <Link href='' target='_blank' style={{ fontSize: '12px' }}>Privacy Policy</Link>{' '}
-                    and{' '}
-                    <Link href='' target='_blank' style={{ fontSize: '12px' }}>Terms & Conditions</Link>
-                  </Text>
-                </Space>
+                    I want to receive updates directly on WhatsApp.
+                  </Checkbox>
+                </Form.Item>
+                <Form.Item>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    style={{
+                      height: "3.5rem",
+                      background:
+                        "linear-gradient(to right, #b132bc 0%, #e4753b 100%",
+                    }}
+                    size="large"
+                    block
+                    loading={submitLoading}
+                  >
+                    Submit
+                  </Button>
+                </Form.Item>
+                <Text
+                  style={{
+                    fontSize: "12px",
+                    textAlign: "center",
+                    color: "#969696",
+                  }}
+                >
+                  By tapping submit, you agree to KnowledgeHut{" "}
+                  <Link href="" target="_blank" style={{ fontSize: "12px" }}>
+                    Privacy Policy
+                  </Link>{" "}
+                  and{" "}
+                  <Link href="" target="_blank" style={{ fontSize: "12px" }}>
+                    Terms & Conditions
+                  </Link>
+                </Text>
               </Form>
             </Col>
           </Row>
